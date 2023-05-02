fa04d5b8161c0d96ad98dc7345131bc156f40de2
TScar <tscarlett@live.com.au>
2023-05-02 13:09:03 +1000

    making app ready for heroku

--- Develop/routes/server.js
+++ Develop/routes/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const uuid = require('../helpers/uuid');
 const notes = require("../db/db.json")
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //middleware
